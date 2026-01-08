3cdccc4b63152c55ab2c958493f2559e8afbc559
hj <parkhj062@gmail.com>
2026-01-08 21:47:29 +0900

    [68. 상품 수정]
    - return "redirect:/../{itemId}"

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -78,6 +78,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId,
+                             @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}"; // use @PathVariable value
+    }
+
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("test1", 1000, 10));
